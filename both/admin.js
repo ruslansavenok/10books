0e95ef05d0e1a59da33c1a14917e23c04f56fce6
Ruslan <ruslan.savenok@gmail.com>
2016-07-31 01:30:46 +0300

    Fix users listing

--- both/admin.js
+++ both/admin.js
@@ -1,1 +1,10 @@
 AdminConfig = {}
+
+// Fix User listing
+// https://github.com/yogiben/meteor-admin/issues/293
+Meteor.startup(function () {
+  const emailCol = AdminTables.Users.options.columns[1]
+  emailCol.render = function(value) {
+    return value && value[0] ? value[0].address : 'no email';
+  }
+})
