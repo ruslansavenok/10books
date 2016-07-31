1e63a818b380c1086b59d78990638a16beba98f0
Ruslan <ruslan.savenok@gmail.com>
2016-07-31 22:11:34 +0300

    Added sentry

---
+++ both/sentry.js
@@ -0,0 +1,4 @@
+RavenLogger.initialize({
+  client: 'http://987d173cd3ce4a8eb692ba48ef2f41ad@sentry.leadfinder.pl/5',
+  server: 'http://14c53f89c1bb4e92a69861239a84dd15:3bc6ae058e2240c9922fc9bdb6cce8b4@sentry.leadfinder.pl/4'
+});
